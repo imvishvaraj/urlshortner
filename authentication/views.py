6d9cb1d536b0f4da70982d8de35c8bf57e265663
Vishvaraj Dhanawade <vishvarajdhnwd120@gmail.com>
2021-09-10 19:16:03 +0530

    added signup login model

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        # handle sign up
+        if request.POST['password'] == request.POST['password2']:
+            if request.POST['username'] and request.POST['email'] and request.POST['password']:
+                try:
+                    user = User.objects.get(email=request.POST['email'])
+                    return render(request, 'signup.html', {'error': "User Already Exists!"})
+                except User.DoesNotExist:
+                    # create user if does not exists
+                    User.objects.create_user(
+                        username=request.POST['username'],
+                        email=request.POST['email'],
+                        password=request.POST['password']
+                    )
+                    messages.success(request, 'Signup Successful <br> Login Here')
+                    return redirect(login)
+            else:
+                return render(request, 'signup.html', {'error': "Empty Fields"})
+        else:
+            return render(request, 'signup.html', {'error': "Password's Don't Match!"})
+
+    else: 
+        return render(request, 'signup.html')
+
+
+# def logout(request):
+#     return render(request, 'logout.html')
--- urlshortner/urls.py
+++ urlshortner/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from authentication.views import home
+from authentication.views import home, login, signup
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('login/', login, name='login'),
+    path('signup/', signup, name='signup'),
     path('admin/', admin.site.urls),
     path('admin/', admin.site.urls),
 ]
